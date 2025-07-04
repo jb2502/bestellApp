0c293ed9df29be8ce6911560cec6cc828a67465a
Jan Brauner <you@example.com>
2025-07-04 19:25:18 +0200

    feat: overlay basket added for empty basket + added obejcts in data.js

---
+++ scripts/data.js
@@ -0,0 +1,34 @@
+myDishes = [
+    {
+        "name": "Gimbap",
+        "price": 9.90,
+    },
+    {
+        "name": "Ramyeon",
+        "price": 10.90,
+    },
+    {
+        "name": "Mandu",
+        "price": 5.50,
+    },
+    {
+        "name": "Rabokki",
+        "price": 9.90,
+    },
+    {
+        "name": "Sikhye",
+        "price": 6.90,
+    },
+    {
+        "name": "Milkis",
+        "price": 4.90,
+    },
+    {
+        "name": "Soju",
+        "price": 5.50,
+    },
+    {
+        "name": "Makgeolli",
+        "price": 12.90,
+    },
+]
