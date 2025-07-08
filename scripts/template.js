8f44a75fe3e1bdb4ccf9269dcc2cca198511efa1
Jan Brauner <you@example.com>
2025-07-08 20:52:50 +0200

    feat: basket almost finished...some bugfixes are necessary

---
+++ scripts/template.js
@@ -0,0 +1,52 @@
+//hinzufügen des ganzen warenkorbs
+function getTemplateFullBasket(index) {
+    return `<div id="added-dish">
+                        <div class="basket-under-headline">
+                            <strong>${basket[index].name}</strong>
+                        </div>
+                        <div class="choose-basket">
+                            <div class="added-dish">
+                                <div onclick="removeBasket(${index})"><img src="./assets/icons/remove_shopping_cart.png" alt="remove"></div>
+                                <div style="width: 30px; text-align: center;">${basket[index].amount}x
+                                </div>
+                                <div onclick="additionalBasket(${index}))"><img src="./assets/icons/add_shopping_cart.png" alt="add"></div>
+                            </div>
+                            <div>${(basket[index].price * basket[index].amount).toFixed(2).replace(".", ",")} €</div>
+                            <div onclick="deleteBasket(${index})"><img src="./assets/icons/delete.png" alt="delete"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="separator-container"></div>
+                        <div class="costs">
+                            <div>Zwischensumme:</div>
+                            <div>X,XX €</div>
+                        </div>
+                        <div class="costs">
+                            <div>Lieferkosten:</div>
+                            <div>X,XX €</div>
+                        </div>
+                        <div class="costs">
+                            <div><strong>Gesamtsumme:</strong></div>
+                            <div><strong>X,XX €</strong></div>
+                        </div>
+                        <div class="btn-container">
+                            <button>Bestellen <img src="./assets/icons/shopping_cart_checkout.png"
+                                    alt="checkout"></button>
+                        </div>
+                    </div>`
+}
+//wenn dish vorhanden nicht ganzen warenkorb hinzufügen
+function getTemplateBasket(index) {
+    return `<div class="basket-under-headline">
+                            <strong>${basket[index].name}</strong>
+                        </div>
+                        <div class="choose-basket">
+                            <div class="added-dish">
+                                <div onclick="removeBasket(${index})"><img src="./assets/icons/remove_shopping_cart.png" alt="remove"></div>
+                                <div style="width: 30px; text-align: center;" id="amount">${basket[index].amount}x</div>
+                                <div onclick="additionalBasket(${index})"><img src="./assets/icons/add_shopping_cart.png" alt="add"></div>
+                            </div>
+                            <div>${(basket[index].price * basket[index].amount).toFixed(2).replace(".", ",")} €</div>
+                            <div onclick="deleteBasket(${index})"><img src="./assets/icons/delete.png" alt="delete"></div>
+                        </div>`
+}
